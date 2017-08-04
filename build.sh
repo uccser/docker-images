780e6c60d7efb47f7537023446bb44214c4ca109
ravenmaster_001 <hayden.adam.jackson@gmail.com>
2017-08-04 13:36:03 +1200

    Update build script to handle dependencies.

--- build.sh
+++ build.sh
@@ -1,33 +1,68 @@
 #!/bin/bash
 function containsElement() {
   local e
-  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
-  return 1
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 1; done
+  return 0
 }
 
 function build() {
   local image=$1
   local directory=$2
 
+  echo "Building: ${directory}"
   docker build -t "uccser/${image}:`cat ${directory}/VERSION`" $directory
   local build_status=$?
   return ${build_status}
 }
 
-todo=()
-built=()
-for D in *; do
-  if [ -d "${D}" ]; then
-    if build ${D} ${D}; then
-      built+=("${D}")
+function main() {
+  local todo=()
+  local built=()
+  for D in *; do
+    if [ -d "${D}" ]; then
+      todo+=(${D})
+      for subD in ${D}/*; do
+        if [ -d "${subD}" ]; then
+          todo+=(${subD})
+        fi
+      done
     fi
+  done
 
-    # for subD in ${D}/*; do
-    #   if [ -d "${subD}" ]; then
-    #     docker build -t "uccser/${D}:`cat ${D}/${subD}/VERSION`" ${subD}
-    #     built += ("${D}/${subD}")
-    #   fi
-    # done
-  fi
-done
-echo "Built: ${built[*]}"
+  local errored=()
+  local backbuffer=()
+  while [ ! ${#todo[@]} -eq 0 ]; do
+    for D in ${todo[*]}; do
+      local ready=1
+      FILE="${D}/REQUIRES"
+      if [ -f ${FILE} ]; then
+        local requires=`cat ${FILE}`
+        readarray -t ADDR <<<"$requires"
+
+        for i in "${ADDR[@]}"; do
+          if containsElement "${i}" "${built[@]}"; then
+            ready=0
+          fi
+        done
+      fi
+
+      if [ ${ready} -eq 1 ]; then
+        if build ${D} ${D}; then
+          built+=("${D}")
+        else
+          errored+=("${D}")
+        fi
+      else
+        backbuffer+=("${D}")
+      fi
+    done
+    local todo=( "${backbuffer[@]}" )
+    local backbuffer=()
+    echo "TODO: ${todo[*]}"
+  done
+  echo "-----------------------"
+  echo "Built: ${built[*]}"
+  echo "Errored: ${errored[*]}"
+}
+
+main
